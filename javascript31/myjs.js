607dfed15e5f618aede71c082872ea0a44411d68
範雷雷 <95039519+leileifan166@users.noreply.github.com>
2021-12-31 14:10:37 +0900

    Add files via upload

---
+++ javascript31/myjs.js
@@ -0,0 +1,22 @@
+var d = new Date();
+console.log(d);
+
+var d2 = new Date("12/30/2021 11:10:30");
+console.log(d2);
+
+var date = d2.getDate();
+console.log(date);
+var date1 = d.getDate();
+console.log(date1);
+
+var day = d2.getDay();
+console.log(day);
+
+var month = d2.getMonth() + 1;
+console.log(month);
+
+
+
+
+
+
